840dff1315b5a00c65a792bae0289005ddaa2edb
ishita2299 <74055297+ishita2299@users.noreply.github.com>
2023-01-23 05:37:15 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,47 +1,56 @@
 # resource "google_compute_engine" "default"{
 #     name =""
+# # }
+
+
+
+
+# resource "google_compute_network" "vpc_network" {
+#   name                    = "my-custom-mode-network"
+#   auto_create_subnetworks = false
+#   mtu                     = 1460
+# }
+
+# resource "google_compute_subnetwork" "default" {
+#   name          = "my-custom-subnet"
+#   ip_cidr_range = "10.0.1.0/24"
+#   region        = "us-west1"
+#   network       = google_compute_network.vpc_network.id
 # }
 
 
-
-
-resource "google_compute_network" "vpc_network" {
-  name                    = "my-custom-mode-network"
-  auto_create_subnetworks = false
-  mtu                     = 1460
-}
-
-resource "google_compute_subnetwork" "default" {
-  name          = "my-custom-subnet"
-  ip_cidr_range = "10.0.1.0/24"
-  region        = "us-west1"
-  network       = google_compute_network.vpc_network.id
+resource "google_storage_bucket" "gcs_bucket" {
+  name = "test-bucket-random-001123"
 }
 
 
-# Create a single Compute Engine instance
-resource "google_compute_instance" "default" {
-  name         = "ishita-vm"
-  machine_type = "f1-micro"
-  zone         = "us-west4-b"
-  tags         = ["ssh"]
 
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-cloud/ubuntu-latest"
-    }
-  }
-}
 
-resource "google_compute_firewall" "ssh" {
-  name = "allow-ssh"
-  allow {
-    ports    = ["22"]
-    protocol = "tcp"
-  }
-  direction     = "INGRESS"
-  network       = google_compute_network.vpc_network.id
-  priority      = 1000
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["ssh"]
-}
+
+
+# # Create a single Compute Engine instance
+# resource "google_compute_instance" "default" {
+#   name         = "ishita-vm"
+#   machine_type = "f1-micro"
+#   zone         = "us-west4-b"
+#   tags         = ["ssh"]
+
+#   boot_disk {
+#     initialize_params {
+#       image = "ubuntu-cloud/ubuntu-latest"
+#     }
+#   }
+# }
+
+# resource "google_compute_firewall" "ssh" {
+#   name = "allow-ssh"
+#   allow {
+#     ports    = ["22"]
+#     protocol = "tcp"
+#   }
+#   direction     = "INGRESS"
+#   network       = google_compute_network.vpc_network.id
+#   priority      = 1000
+#   source_ranges = ["0.0.0.0/0"]
+#   target_tags   = ["ssh"]
+# }
