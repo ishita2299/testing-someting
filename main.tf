fd9b6cbb3a2a41459dd191ff4250828cd0c0b371
ishita2299 <74055297+ishita2299@users.noreply.github.com>
2023-01-23 04:45:22 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,43 @@
+# resource "google_compute_engine" "default"{
+#     name =""
+# }
+
+
+
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-custom-mode-network"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
+
+resource "google_compute_subnetwork" "default" {
+  name          = "my-custom-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-west1"
+  network       = google_compute_network.vpc_network.id
+}
+# Create a single Compute Engine instance
+resource "google_compute_instance" "default" {
+  name         = "Ishita-vm"
+  machine_type = "f1-micro"
+  zone         = "us-west1-a"
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-cloud/debian-11"
+    }
+  }
+  resource "google_compute_firewall" "ssh" {
+  name = "allow-ssh"
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+  direction     = "INGRESS"
+  network       = google_compute_network.vpc_network.id
+  priority      = 1000
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh"]
+}
