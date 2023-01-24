77ac43173d53514491475244e38c8fbc62707ece
ishita2299 <74055297+ishita2299@users.noreply.github.com>
2023-01-24 08:03:46 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ provider "google" {
 
 terraform {
   required_providers {
-    mycloud = {
+    google = {
       source  = "registry.terraform.io/hashicorp/google"
       # version = "~> 1.0"
     }
