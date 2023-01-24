ce9bf33f3f9378ca6050fe68dd460eb6a32bd6a6
ishita2299 <74055297+ishita2299@users.noreply.github.com>
2023-01-24 08:00:09 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,24 @@ resource "null_resource" "git_clone" {
   }
 }
 
+
+
+provider "google" {
+  project     = "ishita-project-15565"
+  region      = "us-west1"
+  zone        = "us-west1-a"
+}
+
+terraform {
+  required_providers {
+    mycloud = {
+      source  = "registry.terraform.io/hashicorp/google"
+      # version = "~> 1.0"
+    }
+  }
+}
+
+
 resource "google_project_service" "appengine" {
 	project="ishita-project-15565"
 	service="appengine.googleapis.com"
