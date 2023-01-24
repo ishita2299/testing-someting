dcf197e1252b352f49c67f079aa1758979369078
ishita2299 <74055297+ishita2299@users.noreply.github.com>
2023-01-24 15:31:53 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "null_resource" "git_clone" {
 provider "google" {
   project     = "var.project_id"
   region      = "var.region"
-  credentials = "var.GCP_SA_KEY"
+  credentials = "GCP_SA_KEY"
   # zone        = "us-west1-a"
 
 }
