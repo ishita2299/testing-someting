ae3ce3ab5ea8495f26890ed508b7b3bfff6ee587
ishita2299 <74055297+ishita2299@users.noreply.github.com>
2023-01-24 10:40:35 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,11 @@ variable "region" {
   type    = string
   default = "europe-west2"
 }
+
+
+variable "GCP_SA_KEY" {
+    type= string
+    sensitive =true
+    description = "key for google auth"
+  
+}
