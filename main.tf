63790148f954cad4ba9528ac8d0f697673a5612e
ishita2299 <74055297+ishita2299@users.noreply.github.com>
2023-01-31 14:47:30 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 provider "google" {
   project     = "var.project_id"
   region      = "var.region"
-  access_token = "ya29.a0AVvZVspQFrTrqs04m4RBAf8r9eGL-0lS1iBDhwBCP5PB8Rp14496oXIn87shqmg1LJOXpeox-R1GSPWkAuEAkj-RiKwMZNzY7p0Dz1PxQqtKGQ4cW2uNx8mTxZ27GVkUpp33URGXWldBw7FeoUfm-L2s8NIA-6wF-m3lNsbJ1JKjjP9ijwwUxPOxBTWfJRrvMUoDYGXX1kmyRCMF0Qu2cgKiHbBdE46jJEFBpTMaCgYKAUISARMSFQGbdwaIJKQe644vFncMy1DxLxI8rA0238"
+  access_token = "ya29.a0AVvZVsqryPBEVawtreZj5qJ10jFQXaXcQ9EIRCCOUyX4gXoh3waFJXuAx1sKrl4CxPsKXcS6SPr0FbFpt3_5EHBnMgoxozeFzIzZsu2WmWWRk1UW0lMA9uOPT1eBEBErYXdwhxzRUH1CdapX5Fn1ZlgLI4noa0MmaXWjZcKOX_Jac4FjWUMMVeh9KedgW4ImJenYjZU7iKnR2NWQtgNwTpboetkrsXZOG1JLYGAaCgYKAeQSARMSFQGbdwaIEDcMXI7mCNSryUc2K4AyVA0238"
 }
 
 # resource "google_project_service" "appengine" {
@@ -10,17 +10,26 @@ provider "google" {
 # 	service="appengine.googleapis.com"
 # 	disable_dependent_services=false
 # }
+
+
+
+resource "google_project_service" "appengine {
+name          = "singular-node-375112"
+project       = "First App engine"
+location      = "europe-west2"
+force_destroy = true
+}
  
 
-resource "google_project" "my_project" {
-  name       = "singular node 375112"
-  project_id = "singular-node-375112"
+# resource "google_project" "my_project" {
+#   name       = "singular node 375112"
+#   project_id = "singular-node-375112"
   
-}
+# }
 
-resource "google_app_engine_application" "app" {
-  project     = google_project.singular_node.375112
-  location_id = "europe-west2"
-}
+# resource "google_app_engine_application" "app" {
+#   project = google_project.singular_node.375112
+#   location_id = "europe-west2"
+# }
 
 
