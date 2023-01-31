ca098ef4b7de18ab41b7db6fe8ee497f6d4d776f
ishita2299 <74055297+ishita2299@users.noreply.github.com>
2023-01-31 14:54:51 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,20 +5,20 @@ provider "google" {
   access_token = "ya29.a0AVvZVsqryPBEVawtreZj5qJ10jFQXaXcQ9EIRCCOUyX4gXoh3waFJXuAx1sKrl4CxPsKXcS6SPr0FbFpt3_5EHBnMgoxozeFzIzZsu2WmWWRk1UW0lMA9uOPT1eBEBErYXdwhxzRUH1CdapX5Fn1ZlgLI4noa0MmaXWjZcKOX_Jac4FjWUMMVeh9KedgW4ImJenYjZU7iKnR2NWQtgNwTpboetkrsXZOG1JLYGAaCgYKAeQSARMSFQGbdwaIEDcMXI7mCNSryUc2K4AyVA0238"
 }
 
-# resource "google_project_service" "appengine" {
-# 	project="singular-node-375112"
-# 	service="appengine.googleapis.com"
-# 	disable_dependent_services=false
-# }
-
-
-
 resource "google_project_service" "appengine" {
-name          = "singular-node-375112"
-project       = "First App engine"
-location      = "europe-west2"
-force_destroy = true
+	project="First App engine"
+	service="appengine.googleapis.com"
+	disable_dependent_services=false
 }
+
+
+
+# resource "google_project_service" "appengine" {
+# name          = "singular-node-375112"
+# project       = "First App engine"
+# location      = "europe-west2"
+# force_destroy = true
+# }
  
 
 # resource "google_project" "my_project" {
