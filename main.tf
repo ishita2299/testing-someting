abbf5bf942a73cf4d29ca2c3d6dd15bccae6b618
ishita2299 <74055297+ishita2299@users.noreply.github.com>
2023-01-31 12:23:43 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,22 @@ provider "google" {
   access_token = "ya29.a0AVvZVsqLy3hEqnIgKKXWNklFFrfSGVvCobBEed6XVi-6LQ1hfshfCCAbcD_T6u8LUOQlkWDd70PcRPGZxFL7G7-feYL7JFhgX8cJ9GIBuMcLolNv4cpE22jAmKx4uRNqyBGwkAZeqYUD5aLjBghVLm4UABxXLeXKyI8Ocj8-HTsUz4Naca_usxuEWFWW0YkdW6xnZQOLWvcuOd6EJJ_fKqvGBR35DyW8oCcd-ogaCgYKAd0SARMSFQGbdwaIKD7XlDJygy-A9Jz0o8tN3A0238"
 }
 
-resource "google_project_service" "appengine" {
-	project="singular-node-375112"
-	service="appengine.googleapis.com"
-	disable_dependent_services=false
-}
+# resource "google_project_service" "appengine" {
+# 	project="singular-node-375112"
+# 	service="appengine.googleapis.com"
+# 	disable_dependent_services=false
+# }
  
 
+resource "google_project" "my_project" {
+  name       = "singular node 375112"
+  project_id = "singular-node-375112"
+  
+}
+
+resource "google_app_engine_application" "app" {
+  project     = google_project.my_project.singular-node-375112.
+  location_id = "europe-west2"
+}
 
 
